288b30ead006441044cb8623eb3b842de637dffc
Sebastian Böck <sebastian.boeck@ofai.at>
2019-07-23 18:00:04 +0200

    refactor model searching

--- __init__.py
+++ __init__.py
@@ -11,45 +11,60 @@ from __future__ import absolute_import, division, print_function
 import os
 import glob
 
-path = os.path.dirname(__file__)
+MODEL_PATH = os.path.dirname(__file__)
+
+
+def models(pattern, path=MODEL_PATH):
+    """
+    Retrieve all models in path given a file name pattern.
+
+    Parameters
+    ----------
+    pattern : str
+        Pattern to be matched.
+    path : str, optional
+        Path to search for model files.
+
+    Returns
+    -------
+    models : list
+        Sorted list of matching model file names.
+
+    """
+    return sorted(glob.glob('%s/%s' % (path, pattern)))
+
 
 # beats
-BEATS_LSTM = sorted(glob.glob('%s/beats/2016/beats_lstm_[1-8].pkl' % path))
-BEATS_BLSTM = sorted(glob.glob('%s/beats/2015/beats_blstm_[1-8].pkl' % path))
+BEATS_LSTM = models('beats/2016/beats_lstm_[1-8].pkl')
+BEATS_BLSTM = models('beats/2015/beats_blstm_[1-8].pkl')
+BEATS_BTCN = models('beats/2019/beats_btcn_[1-8].pkl')
 
 # downbeats
-DOWNBEATS_BLSTM = sorted(glob.glob(
-    '%s/downbeats/2016/downbeats_blstm_[1-8].pkl' % path))
-DOWNBEATS_BGRU = [
-    sorted(glob.glob(
-        '%s/downbeats/2016/downbeats_bgru_rhythmic_*.pkl' % path)),
-    sorted(glob.glob(
-        '%s/downbeats/2016/downbeats_bgru_harmonic_*.pkl' % path))]
-
+DOWNBEATS_BLSTM = models('downbeats/2016/downbeats_blstm_[1-8].pkl')
+DOWNBEATS_BGRU = [models('downbeats/2016/downbeats_bgru_rhythmic_*.pkl'),
+                  models('downbeats/2016/downbeats_bgru_harmonic_*.pkl')]
 # notes
-NOTES_BRNN = sorted(glob.glob('%s/notes/2013/notes_brnn.pkl' % path))
+NOTES_BRNN = models('notes/2013/notes_brnn.pkl')
 
 # onsets
-ONSETS_RNN = sorted(glob.glob('%s/onsets/2013/onsets_rnn_[1-8].pkl' % path))
-ONSETS_BRNN = sorted(glob.glob('%s/onsets/2013/onsets_brnn_[1-8].pkl' % path))
-ONSETS_BRNN_PP = sorted(glob.glob('%s/onsets/2014/onsets_brnn_pp_[1-8].pkl' %
-                                  path))
-ONSETS_CNN = sorted(glob.glob('%s/onsets/2013/onsets_cnn.pkl' % path))
+ONSETS_RNN = models('onsets/2013/onsets_rnn_[1-8].pkl')
+ONSETS_BRNN = models('onsets/2013/onsets_brnn_[1-8].pkl')
+ONSETS_BRNN_PP = models('onsets/2014/onsets_brnn_pp_[1-8].pkl')
+ONSETS_CNN = models('onsets/2013/onsets_cnn.pkl')
 
 # patterns
-PATTERNS_BALLROOM = sorted(
-    glob.glob('%s/patterns/2013/ballroom_pattern_[34]_4.pkl' % path))
+PATTERNS_BALLROOM = models('patterns/2013/ballroom_pattern_[34]_4.pkl')
 
 # chroma
-CHROMA_DNN = ['%s/chroma/2016/chroma_dnn.pkl' % path]
+CHROMA_DNN = models('chroma/2016/chroma_dnn.pkl')
 
 # chords
-CHORDS_DCCRF = ['%s/chords/2016/chords_dccrf.pkl' % path]
-CHORDS_CNN_FEAT = ['%s/chords/2016/chords_cnnfeat.pkl' % path]
-CHORDS_CFCRF = ['%s/chords/2016/chords_cnncrf.pkl' % path]
+CHORDS_DCCRF = models('chords/2016/chords_dccrf.pkl')
+CHORDS_CNN_FEAT = models('chords/2016/chords_cnnfeat.pkl')
+CHORDS_CFCRF = models('chords/2016/chords_cnncrf.pkl')
 
 # key
-KEY_CNN = ['%s/key/2018/key_cnn.pkl' % path]
+KEY_CNN = models('key/2018/key_cnn.pkl')
 
 # keep namespace clean
-del os, glob, path
+del os, glob
