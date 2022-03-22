f3e1a11ef04c276372e9534a8c8ced51391e4e5b
asantos-6 <andresantos_891@hotmail.com>
2022-03-22 16:41:30 +0000

    Changed __init__.py to include drums models

--- __init__.py
+++ __init__.py
@@ -68,5 +68,21 @@ CHORDS_CFCRF = models('chords/2016/chords_cnncrf.pkl')
 # key
 KEY_CNN = models('key/2018/key_cnn.pkl')
 
+# drums
+DRUMS_CRNN = sorted(glob.glob('%s/drums/2017/drums_crnn5_[0-3].pkl' % path))
+DRUMS_BRNN = sorted(glob.glob('%s/drums/2017/drums_brnn2_[0-3].pkl' % path))
+DRUMS_CNN  = sorted(glob.glob('%s/drums/2017/drums_cnn3_[0-3].pkl' % path))
+
+DRUMS_CRNN_R = sorted(glob.glob('%s/drums/2017/drums_crnn5_rand_[0-2].pkl' % path))
+DRUMS_BRNN_R = sorted(glob.glob('%s/drums/2017/drums_brnn2_rand_[0-2].pkl' % path))
+DRUMS_CNN_R  = sorted(glob.glob('%s/drums/2017/drums_cnn3_rand_[0-2].pkl' % path))  # '%s/drums/2017/drums_cnn3_rand_2.pkl' % path] #
+
+DRUMS_CRNN_8  = sorted(glob.glob('%s/drums/2018/drums_crnn1_O8_S[0-4].pkl' % path))
+DRUMS_CNN_8   = sorted(glob.glob('%s/drums/2018/drums_cnn0_O8_S[0-4].pkl' % path))
+DRUMS_CRNN_18 = sorted(glob.glob('%s/drums/2018/drums_crnn1_O18_S[0-4].pkl' % path))
+DRUMS_CNN_18  = sorted(glob.glob('%s/drums/2018/drums_cnn0_O18_S[0-4].pkl' % path))
+
+# DRUMS_SUPER = DRUMS_CRNN + DRUMS_BRNN + DRUMS_CNN
+
 # keep namespace clean
 del os, glob
